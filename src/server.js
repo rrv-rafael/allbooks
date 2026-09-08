f1ebd96352a78d45b6101efd7d1609381780fe73
Rafael Rodrigues Vitor <68669981+rrv-rafael@users.noreply.github.com>
2026-09-08 19:18:44 -0300

    refactor: altera mensagem de acesso à API

--- src/server.js
+++ src/server.js
@@ -15,8 +15,8 @@ server.use('/public', publicRoutes) //rotas na porta 8000
 
 server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddleware);
 
-server.use(router) //rotas na prota 3000
+server.use(router) //rotas na porta 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("Acessa essa API em http://localhost:8000")
 })
