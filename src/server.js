8d273124540e819b8835affb89402542fa0203d9
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2026-09-08 19:20:39 -0300

    refactor: altera mensagem de acesso à API e adiciona mensagem de boas vindas

--- src/server.js
+++ src/server.js
@@ -15,8 +15,9 @@ server.use('/public', publicRoutes) //rotas na porta 8000
 
 server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddleware);
 
-server.use(router) //rotas na prota 3000
+server.use(router) //rotas na porta 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("Boas vindas à API do AllBooks")
+  console.log("API disponível através da url http://localhost:8000")
 })
